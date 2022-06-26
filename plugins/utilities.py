8905ca1afcd0dbc2b7a6d7c7e3f7cbfbcb794dbe
itsamirk <itsamirk0859@gmail.com>
2022-06-26 14:31:19 +0430

    Changes:
    • on_chat_id_requested and on_id_requested have merged.
    
    • You can get id with username now (with or without @).

--- plugins/utilities.py
+++ plugins/utilities.py
@@ -6,34 +6,31 @@ async def on_id_requested(
     _: pyrogram.client.Client,
     message: pyrogram.types.Message,
 ):
-    if message.reply_to_message:
-        sender = message.reply_to_message.from_user
-    else:
-        sender = message.from_user
 
-    if sender is None:
+    if len(message.command) > 1:
+        if message.command[1][0] == "@":
+            user = await _.get_users(message.command[1])
+            id = user.id
+            name = user.first_name
+        elif await _.get_users(message.command[1]): 
+            user = await _.get_users(message.command[1])
+            id = user.id
+            name = user.first_name
+        else:
+            pass
+    elif message.reply_to_message:
+        id = message.reply_to_message.from_user.id
+        name = message.reply_to_message.from_user.first_name
+    else:
+        id = message.chat.id
+        name = message.chat.title
+
+    if message.from_user is None:
         await message.reply_text("I can't find the sender of this message.")
         return
 
-    if sender.is_self:
+    if message.from_user.is_self:
         return
 
-    await message.reply_text(f"{sender.first_name}'s id: `{sender.id}`", quote=True)
-# kio kio
+    await message.reply_text(f"{name}'s id: `{id}`", quote=True)
 
-# Request chat id with chat_id command
-@pyrogram.client.Client.on_message(pyrogram.filters.command("chat_id"))  # type: ignore
-async def on_chat_id_requested(
-    _: pyrogram.client.Client,
-    message: pyrogram.types.Message,
-):
-    if message.reply_to_message:
-        chat = message.reply_to_message.chat
-    else:
-        chat = message.chat
-
-    if chat is None:
-        await message.reply_text("I can't find the chat of this message.")
-        return
-
-    await message.reply_text(f"{chat.title}'s id: `{chat.id}`", quote=True)
