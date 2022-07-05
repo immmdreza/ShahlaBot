5ca9957b532ee5ac3a64fe66fc2288c89b739338
MohammadReza <ir310022@gmail.com>
2022-07-05 12:50:32 +0430

    Added status and unwarn

--- models/group_admin.py
+++ models/group_admin.py
@@ -4,29 +4,36 @@ from .model_base import ModelBase, dataclass, field
 
 class Permissions(IntFlag):
     Nothing = 0
-    CanWarn = 1
-    """Warn users"""
 
-    CanBan = 2
-    """Can permanently ban users"""
-
-    CanMute = 4
-    """CAn permanently mute users"""
-
-    CanDelete = 8
+    CanDelete = 1
     """Can delete message"""
 
-    CanPin = 16
+    CanPin = 2
     """Can pin message"""
 
-    CanSaveExtra = 32
-    """Can save extra data"""
+    CanWarn = 4
+    """Warn users"""
 
-    CanMiniMute = 64
+    CanMiniMute = 8
     """Can mute users for a short time ( less than one hour )"""
 
-    CanMiniBan = 128
+    CanMiniBan = 16
     """Can ban users for a short time ( less than one hour )"""
+
+    CanBan = 32
+    """Can permanently ban users"""
+
+    CanMute = 64
+    """CAn permanently mute users"""
+
+    CanSaveExtra = 128
+    """Can save extra data"""
+
+    NiniAdmin = CanDelete | CanPin | CanWarn | CanMiniMute | CanMiniBan
+    """Can do everything except ban and mute"""
+
+    Admin = NiniAdmin | CanBan | CanMute | CanSaveExtra
+    """Can do everything"""
 
     @staticmethod
     def all():
@@ -49,6 +56,10 @@ class Permissions(IntFlag):
             Permissions.CanDelete: "Can Delete",
             Permissions.CanPin: "Can Pin",
             Permissions.CanSaveExtra: "Can Save Extra",
+            Permissions.CanMiniMute: "Can Mini Mute",
+            Permissions.CanMiniBan: "Can Mini Ban",
+            Permissions.NiniAdmin: "Nini Admin",
+            Permissions.Admin: "Admin",
         }.get(permissions, str(permissions))
 
 
